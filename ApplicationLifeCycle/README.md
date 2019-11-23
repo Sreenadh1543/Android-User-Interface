b604622e27eb0d6d661a52453f8b0f005ee48162
sreenadh1543@gmail.com <sreenadh1543@gmail.com>
2019-11-23 13:27:01 +0100

    Initial Commit

---
+++ ApplicationLifeCycle/README.md
@@ -0,0 +1,13 @@
+# Application Life Cycle
+
+Concepts to be explored 
+
+> States of an Activity 
+
+> Transitions between the states in an activity with the user action with the application
+
+![alt text](https://miro.medium.com/max/1189/1*fCkVwtp7gJ5JI8gjIxPeTQ.png)
+
+
+
+
---
+++ ApplicationLifeCycle/app/src/main/java/com/example/applicationlifecycle/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.applicationlifecycle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
